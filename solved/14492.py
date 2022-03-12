018e90d19c1e4e79860baebf19d6f11ffa45b785
ufshg <ufshg@naver.com>
2022-03-12 15:18:29 +0900

    220312

--- solved/14492.py
+++ solved/14492.py
@@ -8,13 +8,11 @@ for _ in range(n):
 for _ in range(n):
     B.append(list(map(int, input().split())))
 
-for i in A:
+for i in range(n):
     for j in range(n):
-        for k in B:
-            if i[j] and k[j]:
+        for k in range(n):
+            if A[i][k] and B[k][j]:
                 cnt += 1
                 break
 
 print(cnt)
-
-# 비교문 수정 필요
