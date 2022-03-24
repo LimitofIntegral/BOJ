439b61fb1ffc8c1b750bb30a8393a798bd1363eb
ufshg <ufshg@naver.com>
2022-03-24 14:32:34 +0900

    파이썬 heapq 모듈

---
+++ about/about_heapq.py
@@ -0,0 +1,14 @@
+"""
+
+    파이썬에서는 기본 내장 모듈로 heap을 제공한다.
+    일단은 최소 힙의 형태로 정렬해준다.
+
+    import heapq
+
+    메소드는 다음과 같다
+
+    heappush(heap, item) 힙에 아이템 추가
+    heappop(heap)        힙에서 가장 작은 원소 제거 후 리턴, 비어있으면 에러
+    heapify(list_x)      list_x라는 리스트를 힙으로 변환 
+
+"""
