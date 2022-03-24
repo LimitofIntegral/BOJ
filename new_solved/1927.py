2ddbc034b1d5ff31fc8ca55e38306263fe949522
ufshg <ufshg@naver.com>
2022-03-24 14:32:23 +0900

    220324

---
+++ new_solved/1927.py
@@ -0,0 +1,16 @@
+import sys
+import heapq as h
+input = sys.stdin.readline
+
+heap = []
+
+for _ in range(int(input())):
+    x = int(input())
+
+    if x:
+        h.heappush(heap, x)
+    else:
+        if heap:
+            print(h.heappop(heap))
+        else:
+            print(0)
